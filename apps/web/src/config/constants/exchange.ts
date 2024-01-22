b9653f1074264ae520b8e11a424c929ace84d149
ahmadou5 <ahmadlasauwal@gmail.com>
2024-01-22 05:42:43 +0300

    new6

--- apps/web/src/config/constants/exchange.ts
+++ apps/web/src/config/constants/exchange.ts
@@ -9,6 +9,7 @@ export const ROUTER_ADDRESS: ChainMap<string> = {
   [ChainId.GOERLI]: '0xEfF92A263d31888d860bD50809A8D171709b7b1c',
   [ChainId.BSC]: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
   [ChainId.BSC_TESTNET]: '0xD99D1c33F9fC3444f8101754aBC46c52416550D1',
+  [ChainId.FON]: '0xFBbb45aa806B0b0ec511dc50F334D9376b14cB3b'
 }
 
 // used to construct intermediary pairs for trading
